75c55d2bbdf8242155dc4cfa50b6cac7ce9b92aa
Ariel Barbosa Santos <153242199+ariel-barbosa@users.noreply.github.com>
2024-11-07 21:56:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projetoTarefas
+ Projeto de gerenciamento de tarefas
---
+++ db_tarefas.sql
@@ -0,0 +1,21 @@
+create database db_tarefas;
+use db_tarefas;
+
+create table tbl_usuarios (
+	id int not null primary key auto_increment,
+    nome varchar(255) not null,
+    email varchar(255) not null
+);
+
+create table tbl_tarefas (
+	id int not null primary key auto_increment,
+    descricao varchar(255) not null,
+    setor varchar(255) not null,
+    prioridade varchar(255) not null,
+    tar_status varchar(255) not null,
+    tar_data date not null
+);
+
+ALTER TABLE tbl_tarefas
+ADD constraint fk_id_usuarios
+foreign key (id) references tbl_usuarios(id);
